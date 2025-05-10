61ccd4db47efcc3bac9cdb9dce4f1521422a5847
ingabireol <ingabire069@gmail.com>
2025-05-10 20:13:16 +0200

    implemented notification service

--- src/main/java/edtech/afrilingo/config/SecurityConfiguration.java
+++ src/main/java/edtech/afrilingo/config/SecurityConfiguration.java
@@ -35,7 +35,9 @@ public class SecurityConfiguration {
             "/configuration/security",
             "/swagger-ui/**",
             "/webjars/**",
-            "/swagger-ui.html"};
+            "/swagger-ui.html",
+            "/ws/**",
+            "/ws"};
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
---
+++ src/main/java/edtech/afrilingo/notification/NotificationType.java
@@ -0,0 +1,11 @@
+package edtech.afrilingo.notification;
+
+public enum NotificationType {
+    COURSE_COMPLETED,
+    LESSON_COMPLETED, 
+    QUIZ_COMPLETED,
+    NEW_COURSE_AVAILABLE,
+    ACHIEVEMENT_UNLOCKED,
+    SYSTEM_NOTIFICATION,
+    REMINDER
+} 
