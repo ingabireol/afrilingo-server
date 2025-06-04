f2e7759a5be1829438e4b85e04178823b9f9341e
ingabireol <ingabire069@gmail.com>
2025-06-04 11:54:08 +0200

    security config:Oauth2

--- src/main/java/edtech/afrilingo/config/SecurityConfiguration.java
+++ src/main/java/edtech/afrilingo/config/SecurityConfiguration.java
@@ -41,7 +41,7 @@ public class SecurityConfiguration {
     private final JwtAuthenticationFilter jwtAuthFilter;
     private final AuthenticationProvider authenticationProvider;
     private final LogoutHandler logoutHandler;
-//    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -54,9 +54,9 @@ public class SecurityConfiguration {
                                 .anyRequest()
                                 .authenticated()
                 )
-//                .oauth2Login(httpSecurityOAuth2LoginConfigurer -> httpSecurityOAuth2LoginConfigurer
-//                        .successHandler(oAuth2LoginSuccessHandler)
-//                )
+                .oauth2Login(httpSecurityOAuth2LoginConfigurer -> httpSecurityOAuth2LoginConfigurer
+                        .successHandler(oAuth2LoginSuccessHandler)
+                )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
